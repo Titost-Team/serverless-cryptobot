af8a53b7a7013322ace7dd2715a5ec1c609e6d16
ikhvost <illyahvost@gmail.com>
2020-10-04 18:47:36 +0100

    Initial commit

---
+++ serverless.ts
@@ -0,0 +1,48 @@
+import type {Serverless} from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'serverless-cryptobot'
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: [
+    'serverless-webpack',
+    'serverless-iam-roles-per-function',
+    'serverless-offline'
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+  },
+  functions: {
+    hello: {
+      handler: 'handler.hello',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'hello',
+          }
+        }
+      ]
+    }
+  },
+  configValidationMode: 'warn',
+  app: 'serverless-cryptobot',
+  org: 'titost.team'
+}
+
+module.exports = serverlessConfiguration;
